73cf1e9701706a2c5a630c0d64d282fc598730ce
Scott Hansen <firecat4153@gmail.com>
2014-08-16 22:19:16 -0700

    Version bump to 1.0.1

---
+++ CHANGELOG.rst
@@ -0,0 +1,3 @@
+*Version 1.0.1  2014-08-16*
+
+ - Added '0' key for beginning of line. Updated modifier key handling.
--- setup.py
+++ setup.py
@@ -3,17 +3,17 @@
 from distutils.core import setup
 
 setup(name="tabview",
-      version="1.0",
+      version="1.0.1",
       description="A curses command-line CSV and list (tabular data) viewer",
       long_description=open('README.rst').read(),
       author="Scott Hansen",
       author_email="firecat4153@gmail.com",
       url="https://github.com/firecat53/tabview",
-      download_url="https://github.com/firecat53/tabview/tarball/1.0",
+      download_url="https://github.com/firecat53/tabview/tarball/1.0.1",
       packages=['tabview'],
       scripts=['bin/tabview'],
       package_data={'tabview': ['README.rst']},
       data_files=[('share/doc/tabview',
-                   ['README.rst', 'LICENSE.txt'])],
+                   ['README.rst', 'LICENSE.txt', 'CHANGELOG.rst'])],
       license="MIT",
       )
